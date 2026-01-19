aa0b4932d7f28e1668b7515f1b856eeaecf66ba5
iftykhar <s.m.ifty49@gmail.com>
2026-01-19 23:47:13 +0600

    fix: code added

---
+++ src/app/(account)/profile/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from '@/components/Account/pages/ChangePassword'
+
+const page = () => {
+  return (
+    <div>
+        <ChangePassword />
+    </div>
+  )
+}
+
+export default page
